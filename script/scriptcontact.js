d4c36367c49ea7560abfaeb16e8aebbcb8c7cac7
sarahflorez03 <sarahflorez03@hotmail.com>
2022-09-15 19:35:31 -0300

    intento de formulario 1

--- script/scriptcontact.js
+++ script/scriptcontact.js
@@ -18,7 +18,32 @@
 
 // botonSubmit.onsubmit= alert("Holaa");
 
-function muestraMensaje() {
-    console.log('Gracias por pinchar');
-  }
-  document.getElementsByClassName("buttonsubmit").onsubmit = muestraMensaje;
+// function muestraMensaje() {
+//     console.log('Gracias por pinchar');
+//   }
+//   document.getElementsByClassName("buttonsubmit").onsubmit = muestraMensaje;
+
+
+//  const $form = document.querySelector('.contact')
+
+//  $form.addEventListener('submit', handleSubmit)
+
+// async function handleSubmit (event){
+//   event.preventDefault()
+//   const form= new FormData(this)
+//   const response = await fetch (this.action, {
+//     method: this.method,
+//     body: form,
+//     headers: {
+//       'Accept': 'application/json'
+//     }
+    
+//   }
+
+//   )
+//   if (response.ok){
+//     this.reset()
+//     alert('Gracias por contactarnos!')
+//   }
+// }
+  
